f4903efc6fafcbe9a1a23055fc58f1e20aa8fbbc
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2022-07-06 14:02:25 +0600

    add Question Entity

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import Question from 'entities/Question';
 import './index.scss';
 
 function App() {
     return (
         <div className="App">
-            App
+            <Question />
         </div>
     );
 }
---
+++ src/entities/Question/Question.tsx
@@ -0,0 +1,11 @@
+import Card from 'shared/ui/Card';
+
+const Question = () => {
+    return (
+        <Card>
+            <h1>Question</h1>
+        </Card>
+    );
+};
+
+export default Question;
