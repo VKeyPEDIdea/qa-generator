1b8f2b58294b3177ca672d02c2d52a4306e65fbd
Pavel Karyapkin <karyapkin@gmail.com>
2023-11-22 22:40:54 +0600

    change getProjectList method

--- api/general.js
+++ api/general.js
@@ -1,7 +1,7 @@
 ({
   async getProjectList() {
     try {
-      const items = await fsp.readdir(projects.path, 'utf-8');
+      const items = await fsp.readdir(config.projects.path, 'utf-8');
       console.log(items);
       return items;
     } catch (error) {
