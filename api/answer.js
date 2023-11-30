b2cdda378053a195137dd2c536efddcc6fe74b18
Pavel Karyapkin <karyapkin@gmail.com>
2023-11-30 22:42:22 +0600

    add server api method answer create

--- api/answer.js
+++ api/answer.js
@@ -1,1 +1,14 @@
-db('answer');
+({
+    create: (projectName, { ...answers }) => {
+        const path = `${config.projects.path}/${projectName}/answers`;
+        const files = fs.readdirSync(path);
+        Object.values(answers).forEach(data => {
+            let nextFileName = files.length + 1;
+            fs.writeFile(`${path}/${nextFileName}.json`, JSON.stringify(data), (err) => {
+                if (err) throw err;
+                console.log(`New answer has been added: ${nextFileName}.json`);
+            })
+            nextFileName += 1;
+        });
+    },
+});
